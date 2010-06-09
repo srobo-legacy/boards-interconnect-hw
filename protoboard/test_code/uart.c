c59e5037653a73f0057804a451bf3f4c890b6bf9
Richard Barlow <richard@richardbarlow.co.uk>
2010-06-09 18:25:22 +0100

    Add code to init the two UARTS in AVR, one for PC one for SRIC

--- protoboard/test_code/uart.c
+++ protoboard/test_code/uart.c
@@ -1,4 +1,11 @@
 #include "uart.h"
 
 void uart_init() {
+	/* USART0 (SRIC) 8N1 38.4k*/
+	UCSR0B = _BV(RXCIE0) | _BV(TXEN0) | _BV(RXEN0);
+	UBRRL0 = 12;
+
+	/* USART1 (PC) 8N1 9600 */
+	UCSR1B = _BV(RXCIE1) | _BV(TXEN1) | _BV(RXEN1);
+	UBRRL1 = 51;
 }
