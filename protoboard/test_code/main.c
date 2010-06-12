1451025319dfc51877f50a160fe4e93aa4357e54
Richard Barlow <richard@richardbarlow.co.uk>
2010-06-12 14:07:15 +0100

    Output random data on SRIC UART

--- protoboard/test_code/main.c
+++ protoboard/test_code/main.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "uart.h"
+#include <stdlib.h>
 
 FILE uart1_str = FDEV_SETUP_STREAM(uart_pc_send_byte, NULL, _FDEV_SETUP_WRITE);
 
@@ -9,5 +10,8 @@ int main(void) {
 	stdout = stderr = stdin = &uart1_str;
 
 	printf("Start\n");
+	while(1) {
+		uart_sric_send_byte((uint8_t)rand());
+	}
 	return 0;
 }
