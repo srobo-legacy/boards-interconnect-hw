2f891b33111d6c1afe23e6bd95fea5d1487d0716
Richard Barlow <richard@richardbarlow.co.uk>
2010-06-13 13:53:32 +0100

    Add macros for setting/clearing/blinking a flag LED

--- protoboard/test_code/common.h
+++ protoboard/test_code/common.h
@@ -1,7 +1,10 @@
 #ifndef __COMMON_H
 #define __COMMON_H
 
+#define F_CPU 8000000
+
 #include <avr/io.h>
+#include <util/delay.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -14,5 +17,11 @@
 #else
 #	define HAS_UART_1 0
 #endif
+
+#define FLAG_INIT() DDRB  |= _BV(PB0)
+#define FLAG_ON()   PORTB |= _BV(PB0)
+#define FLAG_OFF()  PORTB &= ~_BV(PB0)
+
+#define BLINK() FLAG_ON(); _delay_ms(10); FLAG_OFF()
 
 #endif /* __COMMON_H */
